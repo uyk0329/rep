caa41e9d71a365e3ddc18fb882c32a6437988173
uyk0329 <uyk0329@gmail.com>
2023-09-13 10:49:06 +0900

    04_example_3.ino
    20233027 - SP2 LED

---
+++ 04_example_3.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+}
+
+int toggle_state(int toggle) {
+  if (!toggle) return 1;
+  else return 0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(++count);
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
